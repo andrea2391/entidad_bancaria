d577c1a835467e3b3bcdc90a05bcddede9b18b87
Andrea <andrea.giraldov@uqvirtual.edu.co>
2024-02-16 23:55:32 -0500

    ...

---
+++ src/main/java/co/edu/uniquindio/EntidadBancaria.java
@@ -0,0 +1,24 @@
+package co.edu.uniquindio;
+
+import java.util.ArrayList;
+
+public class EntidadBancaria {
+
+    private String nombre;
+
+    EntidadBancaria(String nombre){
+
+        this.nombre = nombre;
+    }
+
+    public ArrayList registrarUsuario(){
+        
+        return null;
+    }
+    
+    public String crearNumCuenta(){
+
+        return null;
+    }
+    
+}
